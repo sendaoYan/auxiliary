45f246aad6784d7820e352cc609b0037a17908ab
sendaoYan <yansendao.ysd@alibaba-inc.com>
2024-12-26 15:53:17 +0800

    remove llvm-test-suite

--- xcc-test-suite/fuzz/test.cpp
+++
@@ -1,9 +0,0 @@
-// RUN: %cpp -O2 %s -o %t && %t "hello C++" | %FileCheck %s
-// RUN: %cpp %s %OPTFLAGS -o %t && %t "hello C++" | %FileCheck %s
-//CHECK: hello C++
-
-#include <iostream>
-int main(int argc, char** argv) {
-  std::cout << argv[1] << std::endl;
-  return 0;
-}
